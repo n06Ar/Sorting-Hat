33e1ed21137ee25bab9eb6ce4426d4cb7175599e
NobuhitoArai <nobht.arai@gmail.com>
2020-08-28 23:34:19 +0900

    add prefix setting.

--- src/index.ts
+++ src/index.ts
@@ -3,87 +3,88 @@ import fs from "fs"
 import path from "path"
 import inquirer from "inquirer"
 import chalk from "chalk"
-const figlet = require('figlet')
+const figlet = require("figlet")
 
-import {Class,Student,SortData} from './type'
+import { Class, Student, SortData, Prefix } from "./type"
 
 /**
  * main method.
  * @param args input args.
  * @param flags option.
  */
-const main = (args: string[], flags: meow.TypedFlags<any> & { [p: string]: unknown }) : void => {
-    let data:SortData
+const main = (
+    args: string[],
+    flags: meow.TypedFlags<any> & { [p: string]: unknown }
+): void => {
+    let data: SortData
 
     if (flags.file) {
         // Get
         const filename: string = flags.file as string
         data = getFileData(filename)
-    } else if(args.length > 0 && args[0] === "generate") {
-        //　FIXME: Make template file.
+    } else if (args.length > 0 && args[0] === "generate") {
+        // FIXME: Make template file.
         return
     } else {
         // FIXME: Add No file sorting.
         return
     }
 
-    displaySortingResult(sorting(data))
-}
+    displaySortingResult(sorting(data), data.prefixes)
+};
 
 /**
  * Get sorting settings.
  * @param filename string filename.
  * @return SortData
  */
-const getFileData = (filename: string) : SortData => {
+const getFileData = (filename: string): SortData => {
     let sorting
 
     switch (path.extname(filename)) {
-        case '.json':
-            sorting = JSON.parse(fs.readFileSync(`${filename}`, 'utf8'))
-            break
-        case '.yml':
+        case ".json":
+            sorting = JSON.parse(fs.readFileSync(`${filename}`, "utf8"))
+            break;
+        case ".yml":
             break
         default:
             break
     }
 
     return sorting
-}
+};
 
 /**
  * Assign students to classes.
  * @param data
  */
 const sorting = (data: SortData): Class[] => {
-    let classes: Class[] = []
-    let students : Student[] = []
+    const classes: Class[] = []
+    const students: Student[] = []
 
     let sortedStudentsCount = 0
 
     const classStudentNum = {
-        max: Math.round(data.students.length / data.classNames.length),
+        max: Math.ceil(data.students.length / data.classNames.length),
         min: Math.floor(data.students.length / data.classNames.length),
     }
 
     // Make classes.
-    data.classNames.forEach((value)=>{
+    data.classNames.forEach((value) => {
         classes.push({
             name: value,
-            students: []
+            students: [],
         })
-    })
+    });
 
     // Favored assign.
     data.students.forEach((student) => {
-        const index = classes.findIndex(
-            (c) => {
+        const index = classes.findIndex((c) => {
             return c.name === student.favor.className
-        })
-
+        });
         if (index >= 0) {
             classes[index].students.push(student)
-            sortedStudentsCount++
+            sortedStudentsCount++;
         } else {
             students.push(student)
         }
@@ -92,46 +93,55 @@ const sorting = (data: SortData): Class[] => {
     // Normal assign.
     students.forEach((student) => {
         while (true) {
-            const classRandomIndex =  Math.round(Math.random() * (classes.length - 1))
+            const classRandomIndex = Math.round(
+                Math.random() * (classes.length - 1)
+            )
 
-            if (classes[classRandomIndex].students.length < classStudentNum.min) {
+            if (
+                classes[classRandomIndex].students.length < classStudentNum.min
+            ) {
                 classes[classRandomIndex].students.push(student)
-                sortedStudentsCount++
+                sortedStudentsCount++;
                 break
             } else if (
                 // Assignment of people who cannot be divided
-                sortedStudentsCount >= classes.length * classStudentNum.min
-                && classes[classRandomIndex].students.length < classStudentNum.max
+                sortedStudentsCount >= classes.length * classStudentNum.min &&
+                classes[classRandomIndex].students.length < classStudentNum.max
             ) {
                 classes[classRandomIndex].students.push(student)
-                sortedStudentsCount++
+                sortedStudentsCount++;
                 break
             }
         }
     })
 
     return classes
-}
+};
 
 /**
  *  Display Sorting Result.
  *  @param classes Class
+ *  @param prefix Prefix
  */
-const displaySortingResult =  (classes: Class[])  => {
-    console.log('Result !!')
-    console.log('========================')
-    classes.forEach(c => {
+const displaySortingResult = (classes: Class[], prefixes: Prefix[]) => {
+    console.log("Result !!")
+    console.log("========================")
+    classes.forEach((c) => {
         console.log(c.name)
-        console.log('----------------------')
-        c.students.forEach(value => {
-            console.log(value.name)
-        })
-        console.log('========================')
+        console.log("----------------------")
+        c.students.forEach((value, index) => {
+            if (prefixes !== null && prefixes.length > index) {
+                console.log(`${prefixes[index]}`)
+            }
+            console.log(`${index + 1} : ${value.name}`)
+        });
+        console.log("========================");
     })
 }
 
 (async () => {
-    const cli = meow(`
+    const cli = meow(
+        `
     Usage
       $ sorting-hat
 
@@ -142,14 +152,14 @@ const displaySortingResult =  (classes: Class[])  => {
       $ sorting-hat
       $ sorting-hat -f class.json
 `,
-            {
-                flags: {
-                    file: {
-                        type: "string",
-                        alias: "f"
-                    },
-                }
-            })
-        main(cli.input, cli.flags)
-    }
-)()
+        {
+            flags: {
+                file: {
+                    type: "string",
+                    alias: "f",
+                },
+            },
+        }
+    )
+    main(cli.input, cli.flags)
+})()
--- src/type.ts
+++ src/type.ts
@@ -1,17 +1,32 @@
-
+/**
+ * Student Interface
+ * This interface has Student's name and class name of favor.
+ */
 export interface Student {
-    name: "",
+    name: "";
     favor: {
-        className:string
-    }
+        className: string;
+    };
 }
 
+/**
+ * Prefix is
+ */
+export type Prefix = string;
+
+/**
+ * SortData has output data.
+ */
 export interface SortData {
-    classNames: string[],
-    students: Student[]
+    classNames: string[];
+    students: Student[];
+    prefixes: Prefix[];
 }
 
+/**
+ * Class Data
+ */
 export interface Class {
-    name: string
-    students: Student[]
+    name: string;
+    students: Student[];
 }
